b2ce3bb8d142ca90e25d53bc27fd068b26622add
jerome <jerome.sf@outlook.com>
2024-12-30 21:09:17 +0800

    init

---
+++ src/main/java/org/maodou/MaodouAdminApplication.java
@@ -0,0 +1,13 @@
+package org.maodou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaodouAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaodouAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/maodou/MaodouAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maodou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaodouAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
